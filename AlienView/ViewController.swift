ade17b5c44a56080ef82cc57c8300d4890d2b1a8
D7703_18 <d7703_18@d7703-18ui-imac.local>
2018-04-03 15:37:12 +0900

    add alien image

--- AlienView/ViewController.swift
+++ AlienView/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alienlab: UILabel!
+    @IBOutlet weak var alienimage: UIImageView!
+    var counter = 1
+    var direction = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienimage.image = UIImage(named: "frame1.pag")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func butUp(_ sender: Any) {
+        //counter가 5라면 값을 감소시키기 위해 direction을 0으로 변경
+        if counter == 5{
+            direction = 0
+        }
+            //counter가 1이라면 값을 증가시키기 위해 direction을 1으로 변경
+        else if counter == 1{
+            direction = 1
+        }
+        
+        //direction이 1이면 counter를 1씩 증가
+        if direction == 1{
+            counter = counter + 1
+        }
+            //direction이 0이면 counter를 1씩 감소
+        else if direction == 0{
+            counter = counter - 1
+        }
+        
+        alienimage.image = UIImage(named: "frame\(counter).png")
+        alienlab.text = "\(counter)"
     }
-
-
-}
+    
+    }
 
